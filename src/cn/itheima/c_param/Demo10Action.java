c9275c20d903bd1d77b64d60069289c8f2121733
643245611@qq.com <29274@192.168.109.1>
2019-01-20 16:53:30 +0800

    struts-day02

---
+++ src/cn/itheima/c_param/Demo10Action.java
@@ -0,0 +1,29 @@
+package cn.itheima.c_param;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import com.sun.media.sound.ModelChannelMixer;
+
+import cn.itheima.domain.User;
+
+//struts2如何获得参数-方式2
+public class Demo10Action extends ActionSupport implements ModelDriven<User> {
+	//准备user 成员变量
+	//只能返回一个对象
+	private User user =new User();
+
+	public String execute() throws Exception { 
+		
+		System.out.println(user);
+		
+		return SUCCESS;
+	}
+
+	@Override
+	public User getModel() {
+		return user;
+	}
+
+
+	
+}
